b005738e9d873b73f192dbdb3a2ad64e7aad620c
pol1969 <podberezsky1969@gmail.com>
2019-11-10 13:47:55 +0300

    ch6

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Third-party
+    'crispy_forms',
     # Local
     'users.apps.UsersConfig', 
     'pages.apps.PagesConfig',
@@ -125,8 +127,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_FINDERS = [
+        "django.contrib.staticfiles.finders.FileSystemFinder",
+        "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+        ]
+
 
 AUTH_USER_MODEL = 'users.CustomUser' # new
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# django-cryspy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- pages/urls.py
+++ pages/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
-from .views import HomePageView                                                                                                                                    
-urlpatterns = [                                                                                                                                                     
+from .views import HomePageView, AboutPageView
+urlpatterns = [                              
+        path('about/', AboutPageView.as_view(),name='about'),
         path('', HomePageView.as_view(),name='home'),                                                                                                                   ]
