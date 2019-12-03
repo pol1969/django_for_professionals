e089af1eb6915d1f0facedfac8004cf88a6f4371
pol1969 <podberezsky1969@gmail.com>
2019-12-03 09:54:02 +0300

    ch15

--- books/views.py
+++ books/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.mixins import (
         )
 from django.views.generic import ListView, DetailView
 from .models import Book
+from django.db.models import Q
 
 class BookListView(LoginRequiredMixin,ListView):
     model = Book
@@ -20,3 +21,15 @@ class BookDetailView(
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+    #queryset = Book.objects.filter(title__icontains='ш')
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+                Q(title__icontains= query) 
+                | Q(title__icontains=query)
+                )
